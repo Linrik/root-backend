824514f040e23d3ee50709071f08570cb9964a78
Linrik <77327567+Linrik@users.noreply.github.com>
2022-02-07 13:39:51 +0100

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -25,7 +25,7 @@ passport.use(new LocalStrategy(
       cb(null, { id: user.id, username: user.username });
     });
   });
-  //krister du er en kukk
+
   passport.deserializeUser(function(user, cb) {
     process.nextTick(function() {
       return cb(null, user);
